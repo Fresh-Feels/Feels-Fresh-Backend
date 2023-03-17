27519e53bb6bf18e4c24c3a0d1be33920fcdda32
Huzaifa Shah <huzaifa.work5000@gmail.com>
2023-03-18 00:15:43 +0500

    CORS Error 3

--- index.js
+++ index.js
@@ -18,19 +18,18 @@ app.use(express.static("public"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 // Allow requests from a specific domain
-app.use(
-  cors({
-    origin: "https://famous-pothos-e8338c.netlify.app",
-  })
-);
-
-// Allow the 'authorization' header in preflight response
-app.options(
-  "*",
-  cors({
-    allowedHeaders: "authorization, content-type",
-  })
-);
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://famous-pothos-e8338c.netlify.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 //connecting routes
 app.use("/api", apiRouter);
