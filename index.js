6c3f6d769cc21273a1da75647c51cc9787da5eef
Huzaifa Shah <huzaifa.work5000@gmail.com>
2023-03-18 00:01:27 +0500

    CORS Error 2

--- index.js
+++ index.js
@@ -17,15 +17,23 @@ connect();
 app.use(express.static("public"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "https://famous-pothos-e8338c.netlify.app",
-    ],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:3000",
+//       "https://famous-pothos-e8338c.netlify.app",
+//     ],
+//     credentials: true,
+//   })
+// );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 //connecting routes
 app.use("/api", apiRouter);
